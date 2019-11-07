ce3475c865f35842a07ad4ff3b3d4efe07fd95f7
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-11-06 22:04:11 -0600

    Update database.tf

--- examples/bootstrap-google/database.tf
+++ examples/bootstrap-google/database.tf
@@ -1,10 +1,11 @@
 resource "google_sql_database_instance" "tfe-psql-db" {
-  name = "tfe-psql-db-instance"
+  provider = "google-beta"
+  name = "tfe-psql-db-instance-${random_id.db_name_suffix.hex}"
   database_version = "POSTGRES_9_6"
   region = "us-central1"
-  #depends_on = [
-  #  "module.firewall.google_service_networking_connection.ptfe_vpc"
-  #]
+  depends_on = [
+    "module.firewall.google_service_networking_connection.ptfe_vpc"
+  ]
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
@@ -21,4 +22,27 @@ resource "google_sql_user" "tfe-psql-user" {
   instance = "${google_sql_database_instance.tfe-psql-db.name}"
   host     = "me.com"
   password = "${var.postgresql_password}"
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = "google-beta"
+
+  network       = "${google_compute_network.private_network.self_link}"
+  service       = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = ["${google_compute_global_address.private_ip_address.name}"]
+}
+
+#provider "google-beta"{
+#  region = "us-central1"
+#  zone   = "us-central1-a"
+#}
+
+resource "google_compute_global_address" "private_ip_address" {
+  provider = "google-beta"
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type = "INTERNAL"
+  prefix_length = 16
+  network       = "${module.firewall.google_compute_network}"
 }
